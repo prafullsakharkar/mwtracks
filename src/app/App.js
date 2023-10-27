e09bcc1780c71a66d215b24f4f7b80dd0beaf114
Prafull Sakharkar <prafullsakharkar@gmail.com>
2023-10-27 23:42:19 +0530

    enable Access-Control-Allow-Origin

--- src/app/App.js
+++ src/app/App.js
@@ -19,7 +19,7 @@ import axios from 'axios';
  */
 axios.defaults.baseURL = `${process.env.REACT_APP_API_HOST}`;
 axios.defaults.withCredentials = true
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 // axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
 
